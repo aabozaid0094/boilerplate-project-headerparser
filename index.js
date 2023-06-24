7d8db77f8b4072b329c38df274c652ae5e613d1a
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-06-24 16:44:13 +0300

    Add client data endpoint

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ let addClientData = (req, res, next) => {
 app.use('/api/whoami', addClientData);
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  res.json(req.clientData);
 });
 
 // listen for requests :)
